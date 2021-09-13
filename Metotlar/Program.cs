080d618887311c2db9196a7aa313aa28e20bcac5
zgnzdn <41526772+zgnzdn@users.noreply.github.com>
2021-09-13 16:26:11 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // string[] degerler = new string[] { };
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya Elması";
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("----------------------------------------");
+            }
+
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(product2);
+
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Sepete Eklendi. "+product.Adi);
+
+        }
+    }
+}
